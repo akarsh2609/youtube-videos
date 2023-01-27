a64e25e40ed9753797bde9fee716dba9fe5b17c0
akarsh2609 <akarsh2699@gmail.com>
2023-01-28 00:48:36 +0530

    fix get API

--- youtube_videos/views.py
+++ youtube_videos/views.py
@@ -3,11 +3,12 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from youtube_videos.models import Video
+from youtube_videos.serializers import VideoSerializer
 
 
 @api_view(('GET',))
 def get_page(request, page_number):
     video_list = Video.objects.all().order_by('-published_date')
     paginator = Paginator(video_list, 20)
-    page_obj = paginator.get_page(page_number)
-    return Response({"data": page_obj})
+    videos = paginator.page(page_number)
+    return Response(data=VideoSerializer(videos, many=True).data, status=200)
